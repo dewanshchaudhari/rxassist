91001c4ba0751bb3caebb4cc5d7a3791a6d89d90
Dewansh Chaudhari <codewansh@Dewanshs-MacBook-Pro.local>
2024-05-21 05:46:24 +0530

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
-import { useSession } from "next-auth/react";
-
 import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Home() {
   const router = useRouter();
-  const { status } = useSession();
   React.useEffect(() => {
-    if (status === "authenticated") router.push("/home");
-  }, [router, status]);
+    router.push("/home");
+  }, [router]);
   return <></>;
 }
