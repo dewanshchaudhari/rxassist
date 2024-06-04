0edde7c505753263804e0253e5a4cce7819636da
divyanshupandey <divyanshupandey1702@gmail.com>
2024-06-05 01:51:03 +0530

    Login Dialog

--- src/components/PhoneAuthDrawer.tsx
+++ src/components/PhoneAuthDrawer.tsx
@@ -26,9 +26,20 @@ import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { generateRequestID } from "@/lib/utils";
 import { TRUECALLER } from "@/lib/constants";
+import useMediaQuery from "@/hooks/mediaQuery";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 const requestId = generateRequestID();
 const TRUECALLER_URL = `truecallersdk://truesdk/web_verify?requestNonce=${requestId}&partnerKey=${TRUECALLER.partnerKey}&partnerName=${TRUECALLER.partnerName}&lang=${TRUECALLER.lang}&privacyUrl=${TRUECALLER.privacyUrl}&termsUrl=${TRUECALLER.termsUrl}&loginPrefix=${TRUECALLER.loginPrefix}&loginSuffix=${TRUECALLER.loginSuffix}&ctaPrefix=${TRUECALLER.ctaPrefix}&ctaColor=${TRUECALLER.ctaColor}&ctaTextColor=${TRUECALLER.ctaTextColor}&btnShape=${TRUECALLER.btnShape}&skipOption=${TRUECALLER.skipOption}&ttl=${TRUECALLER.ttl}`;
 export function PhoneAuthDrawer({ open }: { open: boolean }) {
+  const isMobile = useMediaQuery("(max-width: 640px)");
+
   const [phone, setPhone] = React.useState<E164Number>();
   const [showOtpScreen, setShowOtpScreen] = React.useState(false);
   const [error, showError] = React.useState(false);
@@ -95,7 +106,7 @@ export function PhoneAuthDrawer({ open }: { open: boolean }) {
       }
     }
   };
-  return (
+  return isMobile ? (
     <Drawer open={show} onOpenChange={setShow}>
       <DrawerTrigger asChild className="p-4">
         <Button variant="default" className="w-full">
@@ -183,5 +194,95 @@ export function PhoneAuthDrawer({ open }: { open: boolean }) {
         </div>
       </DrawerContent>
     </Drawer>
+  ) : (
+    <Dialog open={show} onOpenChange={setShow}>
+      <DialogTrigger asChild className="p-4">
+        <Button variant="default" className="w-full">
+          Login
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <div className="mx-auto h-48 w-full max-w-sm">
+          <DialogHeader>
+            <DialogTitle className="mb-12 w-full text-center">
+              Login
+            </DialogTitle>
+          </DialogHeader>
+          <div className="p-4 pb-0">
+            <div className="flex h-20 w-full flex-col items-center justify-center gap-4">
+              {!showOtpScreen ? (
+                <>
+                  <div
+                    className="w-full"
+                    onClick={() => {
+                      if (!clicked) {
+                        setKey({ requestId });
+                        window.location.assign(TRUECALLER_URL);
+                        setClicked(true);
+                      }
+                    }}
+                  >
+                    <PhoneInputWithCountrySelect
+                      placeholder="Enter phone number"
+                      value={phone}
+                      onChange={setPhone}
+                      addInternationalOption={false}
+                      defaultCountry="IN"
+                      className="mb-2 flex h-full w-full flex-row items-center justify-between"
+                    />
+                  </div>
+                  <Button
+                    disabled={!isPossiblePhoneNumber(phone ?? "") || isPending}
+                    onClick={sendOtp}
+                    className="w-full"
+                  >
+                    {isPending && (
+                      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    )}
+                    Continue
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <form
+                    onSubmit={(e) => void handleSubmit(e)}
+                    className="flex flex-row items-center justify-between gap-2"
+                  >
+                    <Input
+                      placeholder="OTP"
+                      minLength={6}
+                      type="number"
+                      name="otp"
+                      required
+                      onClick={() => showError(false)}
+                      className={`${error === true ? "border-red-200" : ""}`}
+                    />
+                    <Button type="submit">Login</Button>
+                    <Button
+                      variant="link"
+                      size={"icon"}
+                      disabled={
+                        !isPossiblePhoneNumber(phone ?? "") || isPending
+                      }
+                      onClick={sendOtp}
+                    >
+                      <RefreshCw />
+                    </Button>
+                  </form>
+                  {error && <h2 className="text-red-300">INVALID OTP</h2>}
+
+                  <h1 className="mt-4 flex flex-row items-center justify-between text-sm">
+                    We have sent an OTP on your Phone number.
+                  </h1>
+                </>
+              )}
+            </div>
+          </div>
+          <DialogFooter>
+            <div></div>
+          </DialogFooter>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
